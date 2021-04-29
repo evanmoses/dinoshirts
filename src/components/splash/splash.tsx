733eb3d929b2e4ef6709f5ea4fd3b8a41cfb6faf
Evan Moses <evanmoses@gmail.com>
2021-04-29 14:36:54 -0400

    add splash image to homepage

---
+++ src/components/splash/splash.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./splash.scoped.sass";
+
+function Splash() {
+  return (
+    <div className="splash">
+      <h1>DINO SHIRTS</h1>
+    </div>
+  );
+}
+
+export default Splash;
--- src/routes/homepage/homepage.tsx
+++ src/routes/homepage/homepage.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import "./homepage.sass";
 
 import ItemDirectory from "../../components/item-directory";
+import Splash from "../../components/splash";
 
 function Homepage() {
-  return <ItemDirectory />;
+  return (
+    <>
+      <Splash />
+      <ItemDirectory />
+    </>
+  );
 }
 
 export default Homepage;
