adbdfbc4cc91509edd9c96fe0a5926112f7323cb
Evan Moses <evanmoses@gmail.com>
2021-04-30 20:45:39 -0400

    update app route test file

--- src/utils/test-utils/test-utils.tsx
+++ src/utils/test-utils/test-utils.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from "react";
-import { render, RenderOptions } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
-const renderWithRouter: FC = (ui, { route = "/" } = {}) => {
+const renderWithRouter: FC = (ui: ReactElement, { route = "/" } = {}) => {
   window.history.pushState({}, "Test page", route);
 
   return render(ui, { wrapper: BrowserRouter });
