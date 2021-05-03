810727b7789061be87e282da509be0737b7f06a3
Evan Moses <evanmoses@gmail.com>
2021-05-03 15:52:34 -0400

    add placeholder shirts, thumbnail behaviour

--- src/components/square-thumbnail/square-thumbnail.tsx
+++ src/components/square-thumbnail/square-thumbnail.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
 import "./square-thumbnail.scoped.sass";
+import placeholder from "../../images/shirt-placeholder.png";
 
 function SquareThumb() {
-  return <div className="thumb" />;
+  return (
+    <div className="thumb">
+      <img src={placeholder} />
+    </div>
+  );
 }
 
 export default SquareThumb;
